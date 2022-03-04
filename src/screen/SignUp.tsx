4b995ac4e5f05acc664b6817fd0e5453dc472022
Ahn-Gihwan <ahngh0113@naver.com>
2022-03-05 00:39:01 +0900

    feat: Create SignUp Component

---
+++ src/screen/SignUp.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import {Text, SafeAreaView, View} from 'react-native';
+
+function SignUp() {
+  return (
+    <SafeAreaView>
+      <View>
+        <Text>SignUp</Text>
+      </View>
+    </SafeAreaView>
+  );
+}
+
+export default SignUp;
