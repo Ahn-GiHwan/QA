08bfb913d7dc35f679ba41d32e8242f46fe36f7a
Ahn-Gihwan <ahngh0113@naver.com>
2022-03-05 00:38:45 +0900

    feat: Crate SignIn Component

---
+++ src/screen/SignIn.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import {Text, SafeAreaView, View} from 'react-native';
+
+function SignIn() {
+  return (
+    <SafeAreaView>
+      <View>
+        <Text>SignIn</Text>
+      </View>
+    </SafeAreaView>
+  );
+}
+
+export default SignIn;
