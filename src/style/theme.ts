d867988985dcc0f80befc2841d12d933898a5687
Ahn-Gihwan <ahngh0113@naver.com>
2022-03-05 00:22:19 +0900

    style: Theme color and interface

---
+++ src/style/theme.ts
@@ -0,0 +1,17 @@
+export interface Itheme {
+  bg: string;
+  bgBold: string;
+  text: string;
+}
+
+export const lightTheme = {
+  bg: '#e1ffb1',
+  bgBold: '#aed581',
+  text: 'black',
+};
+
+export const darkTheme = {
+  bg: '#3f51b5',
+  bgBold: '#002984',
+  text: 'white',
+};
