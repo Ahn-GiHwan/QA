409c3f06d2d6c81666bb687b79c05b737f4121a4
Ahn-Gihwan <ahngh0113@naver.com>
2022-03-06 17:36:56 +0900

    feat: Add hideA switch

--- src/screen/Setting.tsx
+++ src/screen/Setting.tsx
@@ -1,13 +1,17 @@
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import React, {useCallback, useEffect, useState} from 'react';
 import {Text, Alert} from 'react-native';
+import {useSelector} from 'react-redux';
 import styled from 'styled-components/native';
 import {useAppDispatch} from '../redux';
 import accountSlice from '../redux/accountSlice';
+import hideASlice from '../redux/hideASlice';
+import {RootState} from '../redux/reducer';
 import {ThemeText, ThemeView} from '../style/common';
 
 const Container = styled(ThemeView)`
   flex: 1;
+  align-items: center;
 `;
 
 const Id = styled(ThemeText)`
@@ -15,12 +19,17 @@ const Id = styled(ThemeText)`
   font-weight: bold;
 `;
 
+const SwitchText = styled(ThemeText)``;
+
+const Switch = styled.Switch``;
+
 const LogoutButton = styled.Pressable``;
 
 const LogoutButtonText = styled(Text)``;
 
 function Setting() {
   const [id, setId] = useState<string | null>('');
+  const isHideA = useSelector((state: RootState) => state.hideA.isHideA);
   const dispatch = useAppDispatch();
 
   useEffect(() => {
@@ -31,6 +40,10 @@ function Setting() {
     getId();
   }, []);
 
+  const onChangeHide = useCallback(() => {
+    dispatch(hideASlice.actions.setChange(!isHideA));
+  }, [dispatch, isHideA]);
+
   const onLogout = useCallback(async () => {
     await AsyncStorage.removeItem('id');
     dispatch(accountSlice.actions.setLogout());
@@ -40,6 +53,8 @@ function Setting() {
   return (
     <Container>
       <Id>{id}</Id>
+      <SwitchText>기본 A 숨기기 </SwitchText>
+      <Switch value={isHideA} onValueChange={onChangeHide} />
       <LogoutButton onPress={onLogout}>
         <LogoutButtonText>로그아웃</LogoutButtonText>
       </LogoutButton>
