cd9f3286614386911847252103690813cc9a9f5b
Ahn-Gihwan <ahngh0113@naver.com>
2022-03-06 14:44:52 +0900

    feat: Add theme of object key

--- src/style/theme.ts
+++ src/style/theme.ts
@@ -1,10 +1,12 @@
 export interface Itheme {
+  theme: string;
   bg: string;
   bgBold: string;
   text: string;
 }
 
 export const lightTheme = {
+  theme: 'white',
   bg: '#aed581',
   // bg: '#8bc34a',
   bgBold: '#aed581',
@@ -12,6 +14,7 @@ export const lightTheme = {
 };
 
 export const darkTheme = {
+  theme: 'black',
   bg: '#3f51b5',
   bgBold: '#002984',
   text: 'white',
