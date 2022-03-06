ec5c212e4d7ab85a203a347efd2ef98f931dcb05
Ahn-Gihwan <ahngh0113@naver.com>
2022-03-06 14:43:16 +0900

    feat: import redux and react-query

--- App.tsx
+++ App.tsx
@@ -2,17 +2,26 @@ import React from 'react';
 import {StatusBar, useColorScheme} from 'react-native';
 import {ThemeProvider} from 'styled-components/native';
 import {NavigationContainer} from '@react-navigation/native';
-import NativeStack from './src/navigation/NativeStack';
 import {lightTheme, darkTheme} from './src/style/theme';
+import {QueryClient, QueryClientProvider} from 'react-query';
+import AppInner from './AppInner';
+import {Provider} from 'react-redux';
+import store from './src/redux';
 
 function App() {
   const isDarkMode = useColorScheme() === 'dark';
 
+  const queryClient = new QueryClient();
+
   return (
     <ThemeProvider theme={isDarkMode ? darkTheme : lightTheme}>
       <NavigationContainer>
-        <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-        <NativeStack />
+        <StatusBar barStyle={'dark-content'} />
+        <QueryClientProvider client={queryClient}>
+          <Provider store={store}>
+            <AppInner />
+          </Provider>
+        </QueryClientProvider>
       </NavigationContainer>
     </ThemeProvider>
   );
