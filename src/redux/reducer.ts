ace52d91f4894e16bc7fe4da4871513ee29ba6a8
Ahn-Gihwan <ahngh0113@naver.com>
2022-03-06 17:10:32 +0900

    feat: Add reducer to hideASlice

--- src/redux/reducer.ts
+++ src/redux/reducer.ts
@@ -1,9 +1,11 @@
 import {combineReducers} from 'redux';
 
 import accountSlice from './accountSlice';
+import hideASlice from './hideASlice';
 
 const rootReducer = combineReducers({
   account: accountSlice.reducer,
+  hideA: hideASlice.reducer,
 });
 
 export type RootState = ReturnType<typeof rootReducer>;
